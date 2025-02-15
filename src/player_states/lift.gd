5dd17323df573811cedcff97a08197376a01b7f3
Fernando Righi <43839098+whatsmynameagain@users.noreply.github.com>
2025-02-14 22:58:52 -0300

    Fix keg throw animation not playing

--- src/player_states/lift.gd
+++ src/player_states/lift.gd
@@ -78,8 +78,7 @@ func _update(_delta) -> void:
 				owner.lifted_object._on_throw()
 				
 				if !object_animation.get_parent().is_class("Enemy"):
-					#object_animation.play("thrown") #test
-					object_animation.play("held")
+					object_animation.play("thrown")
 				await owner.animation.animation_finished
 				emit_signal("finished", "Idle")
 		elif ((Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right")) 
